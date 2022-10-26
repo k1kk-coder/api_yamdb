914f0a8c0d8dacdf8b17799c5b660310dc90675a
Алексндр Разумовский <s-razumovskiy@mail.ru>
2022-10-26 09:45:57 +0300

    almost

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -48,6 +48,11 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title = get_object_or_404(Title, pk=title_id)
         return title.reviews.all()
 
+    def get_permissions(self):
+        if self.request.method == "POST":
+            self.permission_classes = (permissions.IsAuthenticated,)
+        return super(ReviewViewSet, self).get_permissions()
+
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
